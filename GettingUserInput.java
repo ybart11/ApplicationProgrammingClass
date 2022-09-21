e7c31d78f1ccfac35f7e62aaec5488fdb4dfcf43
ybart11 <101437280+ybart11@users.noreply.github.com>
2022-09-21 10:59:58 -0400

    Add files via upload

---
+++ GettingUserInput.java
@@ -0,0 +1,25 @@
+package gsu.edu.CIS3270;
+import java.util.Scanner;
+
+public class GettingUserInput {
+
+	public static void main(String[] args) {
+		// Needed to ask user's input 
+		Scanner input = new Scanner (System.in);
+		
+		// ask user for number 
+		System.out.print("Enter a number: ");
+		int num = input.nextInt();
+		
+		// ask user for name
+		System.out.print("Enter a name: ");
+		String name = input.next();
+		
+		for (int i = 0; i <num; i++) {
+			System.out.println(name + " is the goat");
+		}
+		
+		input.close();
+	}
+
+}
