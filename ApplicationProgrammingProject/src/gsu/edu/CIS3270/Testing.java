afe86bdec8671e63adf3dafc388e702b3f314be1
yovany_23 <yovany_23@yovanys-mac.wifi.gsu.edu>
2022-09-21 17:26:08 -0400

    Added message class

--- ApplicationProgrammingProject/src/gsu/edu/CIS3270/Testing.java
+++ ApplicationProgrammingProject/src/gsu/edu/CIS3270/Testing.java
@@ -4,7 +4,13 @@ public class Testing {
 
 	public static void main(String[] args) {
 		System.out.println("Testing");
-
+		message(); 
+		
+	}
+	
+	public static void message() {
+		System.out.println("My name is Yovany");
 	}
 
-}
+}	
+
