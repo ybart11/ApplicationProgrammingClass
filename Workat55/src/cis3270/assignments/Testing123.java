3aa26f77fd40beb5befd6f8bebd8e613f9381a92
yovany_23 <yovany_23@yovanys-mac.wifi.gsu.edu>
2022-09-22 09:57:06 -0400

    Testing, testing

---
+++ Workat55/src/cis3270/assignments/Testing123.java
@@ -0,0 +1,20 @@
+package cis3270.assignments;
+import java.util.Scanner;
+public class Testing123 {
+
+	public static void main(String[] args) {
+		// Testing to see how this will look in GitHub 
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("What is your favorite number? ");
+		int favNum = input.nextInt();
+		
+		// display results
+		System.out.println("Your favorite number is " + favNum);
+		
+		// close scanner 
+		input.close();
+	}
+
+}
