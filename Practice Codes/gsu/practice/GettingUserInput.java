f853589cbc0663b5f6660a029b6c3acdc30592e7
ybartolome1 <ybartolome1@RCB5047tf-ddba.dmd.gsuad.gsu.edu>
2022-09-28 08:54:09 -0400

    not sure

--- Practice Codes/gsu/practice/GettingUserInput.java
+++ Practice Codes/gsu/practice/GettingUserInput.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 public class GettingUserInput {
 
 	public static void main(String[] args) {
-		// Needed to ask user's input
+		// Needed to ask user's input 
 		Scanner input = new Scanner (System.in);
-
-		// ask user for number
+		
+		// ask user for number 
 		System.out.print("Enter a number: ");
 		int num = input.nextInt();
-
+		
 		// ask user for name
 		System.out.print("Enter a name: ");
 		String name = input.next();
-
+		
 		for (int i = 0; i <num; i++) {
 			System.out.println(name + " is the goat");
 		}
-
+		
 		input.close();
 	}
 
--- Workat55/src/cis3270/assignments/NumberOfYears.java
+++ Workat55/src/cis3270/assignments/NumberOfYears.java
@@ -9,15 +9,15 @@ public class NumberOfYears {
 		int years;
 		int days;
 		Scanner input = new Scanner (System.in);
-
+		
 		System.out.print("Enter the number of minutes: ");
 		int userMin = input.nextInt();
-
+		
 		years = userMin / 60 / 24 / 365;
 		days = userMin / 60 / 24 % 365;
-
+		
 		System.out.println(userMin + " minutes is approximatelty " + years + " year(s) and " + days + " day(s).");
-
+		
 		input.close();
 	}
 
