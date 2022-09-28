3f7cb86b8572ef01bc982bfc82eade6d1e4a7a83
ybartolome1 <ybartolome1@RCB5047tf-ddba.dmd.gsuad.gsu.edu>
2022-09-28 11:00:31 -0400

    new class

---
+++ Workat55/src/practice/Yer.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Yer {
+
+	public static void main(String[] args) {
+		// second pic from phone
+		int [] numbers = {3, 4, 5, 7, 9, 2}; 
+		
+		for (int num : numbers) { 
+			System.out.print(num + " ");
+		}
+
+	}
+
+}
