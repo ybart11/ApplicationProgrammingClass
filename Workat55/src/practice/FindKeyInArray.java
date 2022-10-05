4f19f10740811eb52227b772875e99153cb14338
yovany_23 <yovany_23@yovanys-mac.wifi.gsu.edu>
2022-10-04 23:49:05 -0400

    new practice from tuesday's lecture

---
+++ Workat55/src/practice/FindKeyInArray.java
@@ -0,0 +1,12 @@
+package practice;
+import java.util.Arrays;
+
+public class FindKeyInArray {
+
+	public static void main(String[] args) {
+
+		System.out.println(Arrays.binarySearch(new int [] {1,3,5,7,9}, 7)); // will return -6 because it should be 
+
+	}
+
+}
---
+++ Workat55/src/practice/Sorting.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int [] list = {9, 8, 4, 2, 6, 3, 5, 1};
+		
+		Arrays.sort(list);
+		
+		for (int num : list) {
+			System.out.print(num + " ");
+	}
+
+}
+}
