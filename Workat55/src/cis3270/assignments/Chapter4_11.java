69327704e29face05a5b3b1ac274e5a85cd88106
yovany_23 <yovany_23@yovanys-mac.wifi.gsu.edu>
2022-10-11 15:44:13 -0400

    robby class

--- Workat55/src/cis3270/assignments/Chapter4_11.java
+++ Workat55/src/cis3270/assignments/Chapter4_11.java
@@ -3,7 +3,16 @@ package cis3270.assignments;
 public class Chapter4_11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String name = "carlos";
+		
+		String name1 = " messi     ";
+				
+		System.out.print(name.concat(name1));
+		
+
+		
+		
 
 	}
 
