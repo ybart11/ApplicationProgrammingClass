54ef3816f5cc4819cb24222223e74bf10cc75a55
ybartolome1 <ybartolome1@RCB6449TF-DDBA.dmd.gsuad.gsu.edu>
2022-10-13 10:31:00 -0400

    chapter 5.21

--- Workat55/src/assignments/chapter5/Chapter5_18.java
+++ Workat55/src/assignments/chapter5/Chapter5_18.java
@@ -3,11 +3,11 @@ package assignments.chapter5;
 public class Chapter5_18 {
 
 	public static void main(String[] args) {
-		// Program A
+//		// Program A
 		System.out.println("Pattern A");
-		for (int i = 1; i <= 6; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
+		for (int r = 1; r <= 6; r++) {
+			for (int c = 1; c <= r; c++) {
+				System.out.print(c + " ");
 			}
 			System.out.println();
 		}
@@ -18,13 +18,48 @@ public class Chapter5_18 {
 		
 		
 		// Program B
-		
-		for (int i = 6; i <= 1; i--) {
-			for (int j = 6; j <= i; j--) {
-				System.out.print(j + " ");
+		System.out.println("Pattern B");
+		for (int r = 6; r >= 1; r--) {
+			for (int c = 1; c <= r; c++) {
+				System.out.print(c + " ");
 			}
 			System.out.println();
 		}
+		
+		System.out.println();
+
+		
+		// Program C
+		System.out.println("Pattern C");
+		int numLines = 6;
+		for (int r = 1; r <= numLines; r++) {
+			for (int s = numLines - r; s >= 1; s--) {
+				System.out.print("  "); // gives it the right shape
+			}
+			
+			for (int c = r; c >= 1; c--) {
+				System.out.print(c + " "); // displays the integers in the right order
+			}
+			System.out.println();
+		}
+		
+		
+		// Program D
+		System.out.println("\nPattern D");
+		for (int r = 0; r < 6; r++) {
+			for (int s = 0; s < r; s++) {
+				System.out.print("  "); // gives it the right shape 
+			}
+			
+			for (int c = 0; c < 6 - r; c++) {
+				System.out.print((c + 1) + " "); // displays the integers in the right order
+			}
+			
+			System.out.println();
+
+		}
+
+		
 	}
 
 }
