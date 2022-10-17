382d3ae0bc9e9451c553200b20460c69353ba521
yovany_23 <yovany_23@192.168.0.14>
2022-10-17 12:45:31 -0400

    Chapter 7.1 done

---
+++ Workat55/src/practice/Calculator.java
@@ -0,0 +1,41 @@
+package practice;
+
+public class Calculator {
+	
+	/** Main method */
+	public static void main(String[] args) {
+		// Check number of strings passed
+		if (args.length != 3 ) {
+			System.out.println("Usage: java Calculator operand1 operator operand2");
+			System.exit(1);
+		}
+
+	
+	
+	// The result of the operation 
+	int result = 0;
+	
+	// Determine the operator
+	switch (args[1].charAt(0)) {
+	case '+': result = Integer.parseInt(args[0]) + 
+			 		   Integer.parseInt(args[2]);
+					   break;
+	case '-': result = Integer.parseInt(args[0]) - 
+	 		   Integer.parseInt(args[2]);
+			   break;
+					   
+	case '.': result = Integer.parseInt(args[0]) * 
+	 		   Integer.parseInt(args[2]);
+			   break;			   
+	case '/': result = Integer.parseInt(args[0]) / 
+	 		   Integer.parseInt(args[2]);
+			   // do not need to add break since it is the last case 				   			   
+	}
+	
+	// Display result
+	System.out.println(args[0] + " " + args[1] + " " + args[2]
+		+ " = " + result);
+	
+	
+	}
+}
