85b22525e29017fd3f84c511fb2cc006621e07d3
yovany_23 <yovany_23@yovanys-mac.wifi.gsu.edu>
2022-10-25 20:06:50 -0400

    cat class created

---
+++ Workat55/src/practice/classes/objects/AwesomeJavaProgram.java
@@ -0,0 +1,18 @@
+package practice.classes.objects;
+
+public class AwesomeJavaProgram {
+
+	public static void main(String[] args) {
+		// display result of class
+
+		Cat cookie = new Cat();
+		
+		cookie.age = 10;
+		cookie.name = "Cookie";
+		
+		
+		
+		
+	}
+
+}
---
+++ Workat55/src/practice/classes/objects/Cat.java
@@ -0,0 +1,15 @@
+package practice.classes.objects;
+
+public class Cat {
+	int age; 
+	String name; 
+	
+	public void meow() {
+		System.out.println("Meow");
+	}
+	
+	public static void dingDong() {
+		System.out.println("Ding dong");
+	}
+	
+}
