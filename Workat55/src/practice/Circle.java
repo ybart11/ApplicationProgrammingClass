331593b748c830e3674bd122b873aa1d57d5bbc2
yovany_23 <yovany_23@localhost>
2022-11-07 23:50:03 -0500

    More practice

---
+++ Workat55/src/practice/Circle.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Circle {
+	public int count;
+	
+	double radius;
+	
+	Circle (double newRadius) {
+		radius = newRadius; 
+	}
+}
--- Workat55/src/practice/Yer.java
+++ Workat55/src/practice/Yer.java
@@ -1,14 +1,33 @@
 package practice;
-
+//import java.util.Date;
 
 public class Yer {
 	public static void main(String [] args) {
-		printHeader();
-
+	
+		Circle circle1 = new Circle(1); 
+		Circle circle2 = new Circle(2);
+		
+		swap1 (circle1, circle2);
+		System.out.println("After swap1: circle1 = " + circle1.radius +
+				" circle2 = " + circle2.radius);
+		
+		swap2 (circle1, circle2);
+		System.out.println("After swap2: circle1 = " + 
+		circle1.radius + " circle2 = " + circle2.radius);
 	}
 	
-	public static void printHeader() {
-		System.out.println("Name: millie");
+	public static void swap1 (Circle x, Circle y) {
+		Circle temp = x; // temp = 1
+		x = y; // x = 2
+		y = temp; // y = 1  
 	}
+	
+	public static void swap2 (Circle x, Circle y) {
+		double temp = x.radius; 
+		x.radius = y.radius; 
+		y.radius = temp;
+	}
+	
+
 }
 	
